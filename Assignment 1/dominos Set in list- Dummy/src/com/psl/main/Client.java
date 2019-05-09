bcdf85c5af19ae6f9dfa970375668d9274ecad0b
Varun Shanbhag <shanbhagvarun55@gmail.com>
2019-05-10 01:32:38 +0530

    Method 1 Solution

--- Assignment 1/dominos Set in list- Dummy/src/com/psl/main/Client.java
+++ Assignment 1/dominos Set in list- Dummy/src/com/psl/main/Client.java
@@ -7,14 +7,23 @@ import java.util.Set;
 
 import com.psl.bean.Dish;
 import com.psl.bean.Location;
-import com.psl.bean.Order;
-import com.psl.util.DominozPizzaDelivery;
 import com.psl.util.DominozPizzaDeliveryImpl;
 
 public class Client {
 
+	private static final String dishFile = "C:\\Users\\Varun\\Desktop\\Varun\\Amar_Workspace\\MyCode\\Assignment 1\\dominos Set in list- Dummy\\dish.txt";
+	private static final String locationFile = "C:\\Users\\Varun\\Desktop\\Varun\\Amar_Workspace\\MyCode\\Assignment 1\\dominos Set in list- Dummy\\location.txt";
+
 	public static void main(String[] args) {
-	
+
+		List<Dish> dishes = new ArrayList<>();
+		Set<Location> locations = new HashSet<>();
+
+		DominozPizzaDeliveryImpl dominozPizzaDelivery = new DominozPizzaDeliveryImpl();
+
+		dominozPizzaDelivery.populateData(dishFile,locationFile,dishes,locations);
+
+
 	}
 
 }
--- Assignment 1/dominos Set in list- Dummy/src/com/psl/util/DominozPizzaDeliveryImpl.java
+++ Assignment 1/dominos Set in list- Dummy/src/com/psl/util/DominozPizzaDeliveryImpl.java
@@ -1,11 +1,9 @@
 package com.psl.util;
 
+import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileNotFoundException;
-import java.util.ArrayList;
-import java.util.HashSet;
+import java.io.FileReader;
 import java.util.List;
-import java.util.Scanner;
 import java.util.Set;
 
 import com.psl.bean.Dish;
@@ -15,8 +13,65 @@ import com.psl.bean.Order;
 
 public class DominozPizzaDeliveryImpl implements DominozPizzaDelivery {
 
-	
 
+    @Override
+    public void populateData(String dishFile, String locationFile, List<Dish> dishs, Set<Location> locations) {
+        File dishFileObject = new File(dishFile);
 
-	
+        BufferedReader br = null;
+        try {
+            br = new BufferedReader(new FileReader(dishFileObject));
+            String dishLine;
+            while ((dishLine = br.readLine()) != null){
+                Dish dish = new Dish();
+                String[] splitedLine = dishLine.split(",");
+                dish.setDishId(Integer.valueOf(splitedLine[0]));
+                dish.setDishName(splitedLine[1]);
+                dish.setCost(Double.valueOf(splitedLine[2]));
+                dish.setTimeToCook(Double.valueOf(splitedLine[3]));
+
+                dishs.add(dish);
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        File locationFileObject = new File(locationFile);
+
+        BufferedReader br1 = null;
+        try {
+            br1 = new BufferedReader(new FileReader(locationFileObject));
+            String locationLine;
+            while ((locationLine = br1.readLine()) != null){
+                Location location = new Location();
+                String[] splitedLine = locationLine.split(",");
+
+                location.setLocationCode(Integer.valueOf(splitedLine[0]));
+                location.setLocationDistance(Integer.valueOf(splitedLine[1]));
+                location.setLocationTime(Double.valueOf(splitedLine[2]));
+
+                locations.add(location);
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    @Override
+    public void calculateLocationForDistance(List<Dish> dishs, Set<Location> locations) {
+
+    }
+
+    @Override
+    public List<Order> calculateOrder(String orderFile, List<Dish> dishs, Set<Location> locations) {
+        return null;
+    }
+
+    @Override
+    public void freeDelivery(List<Order> orders, List<Dish> dishs, Set<Location> locations) {
+
+    }
 }
